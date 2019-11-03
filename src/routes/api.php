3c2ce3ea50972b0228b9f3635830e7284ff595d2
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-11-03 14:40:29 -0300

    Correção das rotas para API REST com e sem auth

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,11 @@
 <?php
-Route::prefix('api/pagseguro')->middleware('auth:api')->name('api.pagseguro.')
-  ->namespace('Acarolinafg\PagSeguro\Http\Controllers')->group(function () {
-    Route::get('session', 'PagSeguroCheckoutTransparenteController@session')->name('session');
-    Route::get('javascript', 'PagSeguroCheckoutTransparenteController@javascript')->name('javascript');
-    Route::get('javascript-content', 'PagSeguroCheckoutTransparenteController@javaScriptContent')->name('javaScriptContent');
-  });
+Route::group([
+  'namespace' => 'Acarolinafg\PagSeguro\Http\Controllers',
+  'middleware' => ['auth:api'],
+  'name' => 'api.v2.pagseguro',
+  'prefix'=>'pagseguro/api/v2'
+], function () {
+  Route::get('session', 'PagSeguroCheckoutTransparenteController@session')->name('session');
+  Route::get('javascript', 'PagSeguroCheckoutTransparenteController@javascript')->name('javascript');
+  Route::get('javascript-content', 'PagSeguroCheckoutTransparenteController@javaScriptContent')->name('javaScriptContent');
+});
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,10 @@
 <?php
-Route::prefix('pagseguro')->middleware('web')->name('api.pagseguro.')
-->namespace('Acarolinafg\PagSeguro\Http\Controllers')->group(function () {
+Route::group([
+  'namespace' => 'Acarolinafg\PagSeguro\Http\Controllers',
+  'middleware' => ['web'],
+  'name' => 'pagseguro',
+  'prefix'=>'pagseguro'
+], function () {
   Route::get('session', 'PagSeguroCheckoutTransparenteController@session')->name('session');
   Route::get('javascript', 'PagSeguroCheckoutTransparenteController@javascript')->name('javascript');
   Route::get('javascript-content', 'PagSeguroCheckoutTransparenteController@javaScriptContent')->name('javaScriptContent');
