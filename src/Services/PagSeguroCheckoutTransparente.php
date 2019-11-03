58fe47a81bd49d37d654c5df5115ea7414dd599c
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-11-03 15:49:27 -0300

    Método de inicio de sessão passa a ser da classe client, pois é utilizado o mesmo para outros serviços

--- src/Services/PagSeguroCheckoutTransparente.php
+++ src/Services/PagSeguroCheckoutTransparente.php
@@ -2,30 +2,15 @@
 
 namespace Acarolinafg\PagSeguro\Services;
 
+use Acarolinafg\PagSeguro\Classes\Shopping;
+
 /**
  * Classe de Checkout Transparente do PagSeguro
  * @author Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
  */
 class PagSeguroCheckoutTransparente extends PagSeguroClient
 {
-  /**
-   * Inicia uma sessão para o Checkout
-   * @return string
-   */
-  public function startSession()
-  {
-    //definição dos parâmetros
-    $param = ['email' => $this->email, 'token' => $this->token];
-    $this->parameters = pagseguro_str_parameters($param);
-
-    //definição da URL
-    $this->url = $this->getUrl('checkout.session');
-
-    //enviar requisição
-    $this->sendRequest();
-
-    return (string) $this->result->id;
-  }
+  use Shopping; //Dados da compra
 
   /**
    * Cancelar transação
