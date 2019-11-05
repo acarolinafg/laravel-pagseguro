aeeec483fbcb60733fbcb5a23ecd29bbc89f96f5
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-11-04 22:13:32 -0300

    Criação do modelo de endereço de pagamento e frete

---
+++ src/Classes/BillingAddress.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Acarolinafg\PagSeguro\Classes;
+
+/**
+ * Classe BillingAddress - Endereço de cobrança
+ *
+ * @author Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
+ */
+class BillingAddress extends CommonData
+{
+  /**
+   * Nome da rua do endereço
+   * @var string
+   */
+  protected $street;
+
+  /**
+   * Número do endereço
+   * @var string
+   */
+  protected $number;
+
+  /**
+   * Bairro do endereço
+   * @var string
+   */
+  protected $district;
+
+  /**
+   * Cidade do endereço
+   * @var string
+   */
+  protected $city;
+
+  /**
+   * Estado do endereço
+   * @var string
+   */
+  protected $state;
+
+  /**
+   * País do endereço
+   * @var string
+   */
+  protected $country = 'BRA';
+
+  /**
+   * CEP do endereço
+   * @var string
+   */
+  protected $postalCode;
+
+  /**
+   * Complemento do endereço
+   * @var string
+   */
+  protected $complement;
+
+  public function __construct(array $data)
+  {
+    $this->alias = 'billingAddress';
+    $this->attributes = ['street', 'number', 'district', 'city', 'state', 'postalCode', 'complement'];
+    parent::__construct($data);
+  }
+
+  public function getStreet()
+  {
+    return $this->street;
+  }
+
+  public function setStreet($street)
+  {
+    $this->street = pagseguro_clear_value($street);
+  }
+
+  public function getNumber()
+  {
+    return $this->number;
+  }
+
+  public function setNumber($number)
+  {
+    $this->number = pagseguro_clear_value($number);
+  }
+
+  public function getDistrict()
+  {
+    return $this->district;
+  }
+
+  public function setDistrict($district)
+  {
+    $this->district = pagseguro_clear_value($district);
+  }
+
+  public function getCity()
+  {
+    return $this->city;
+  }
+
+  public function setCity($city)
+  {
+    $this->city = pagseguro_clear_value($city);
+  }
+
+  public function getCountry()
+  {
+    return $this->country;
+  }
+
+  public function getState()
+  {
+    return $this->state;
+  }
+
+  public function setState($state)
+  {
+    $this->state = pagseguro_clear_value($state);
+  }
+
+  public function getPostalCode()
+  {
+    return $this->postalCode;
+  }
+
+  public function setPostalCode($postalCode)
+  {
+    $this->postalCode = pagseguro_clear_number($postalCode);
+  }
+
+  public function getComplement()
+  {
+    return $this->complement;
+  }
+
+  public function setComplement($complement)
+  {
+    $this->complement = pagseguro_clear_value($complement);
+  }
+
+  public function rules(): array
+  {
+    return [
+      'street'     => 'required|max:80',
+      'number'     => 'required|max:20',
+      'district'   => 'required|max:60',
+      'city'       => 'required|min:2|max:60',
+      'state'      => 'required|min:2|max:2',
+      'postalCode' => 'required|digits:8',
+      'complement' => 'required|max:40'
+    ];
+  }
+}
---
+++ src/Classes/Shipping.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Acarolinafg\PagSeguro\Classes;
+
+/**
+ * Classe Shipping - Frete
+ *
+ * @author Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
+ */
+class Shipping extends CommonData
+{
+  /**
+   * Nome da rua do endereço
+   * @var string
+   */
+  protected $street;
+
+  /**
+   * Número do endereço
+   * @var string
+   */
+  protected $number;
+
+  /**
+   * Bairro do endereço
+   * @var string
+   */
+  protected $district;
+
+  /**
+   * Cidade do endereço
+   * @var string
+   */
+  protected $city;
+
+  /**
+   * Estado do endereço
+   * @var string
+   */
+  protected $state;
+
+  /**
+   * País do endereço
+   * @var string
+   */
+  protected $country = 'BRA';
+
+  /**
+   * CEP do endereço
+   * @var string
+   */
+  protected $postalCode;
+
+  /**
+   * Complemento do endereço
+   * @var string
+   */
+  protected $complement;
+
+  /**
+   * Valor do frete
+   * @var string
+   */
+  protected $cost;
+
+  public function __construct(array $data)
+  {
+    $this->alias = 'shipping';
+    $this->attributes = ['street', 'number', 'district', 'city', 'state', 'postalCode', 'complement', 'cost'];
+    parent::__construct($data);
+  }
+
+  public function getStreet()
+  {
+    return $this->street;
+  }
+
+  public function setStreet($street)
+  {
+    $this->street = pagseguro_clear_value($street);
+  }
+
+  public function getNumber()
+  {
+    return $this->number;
+  }
+
+  public function setNumber($number)
+  {
+    $this->number = pagseguro_clear_value($number);
+  }
+
+  public function getDistrict()
+  {
+    return $this->district;
+  }
+
+  public function setDistrict($district)
+  {
+    $this->district = pagseguro_clear_value($district);
+  }
+
+  public function getCity()
+  {
+    return $this->city;
+  }
+
+  public function setCity($city)
+  {
+    $this->city = pagseguro_clear_value($city);
+  }
+
+  public function getCountry()
+  {
+    return $this->country;
+  }
+
+  public function getState()
+  {
+    return $this->state;
+  }
+
+  public function setState($state)
+  {
+    $this->state = pagseguro_clear_value($state);
+  }
+
+  public function getPostalCode()
+  {
+    return $this->postalCode;
+  }
+
+  public function setPostalCode($postalCode)
+  {
+    $this->postalCode = pagseguro_clear_number($postalCode);
+  }
+
+  public function getComplement()
+  {
+    return $this->complement;
+  }
+
+  public function setComplement($complement)
+  {
+    $this->complement = pagseguro_clear_value($complement);
+  }
+
+  public function getCost()
+  {
+    return $this->cost;
+  }
+
+  public function setCost($cost)
+  {
+    $this->cost = pagseguro_format_money($cost);
+  }
+
+  public function rules(): array
+  {
+    return [
+      'street'     => 'required|max:80',
+      'number'     => 'required|max:20',
+      'district'   => 'required|max:60',
+      'city'       => 'required|min:2|max:60',
+      'state'      => 'required|min:2|max:2',
+      'postalCode' => 'required|digits:8',
+      'complement' => 'nullable|max:40',
+      'cost'       => 'required|between:0.00,9999999.00'
+    ];
+  }
+
+  public function toArray(bool $useAlias = false): array
+  {
+    if ($useAlias) {
+
+      $array['shippingAddressStreet'] = $this->getStreet();
+      $array['shippingAddressNumber'] = $this->getNumber();
+      $array['shippingAddressDistrict'] = $this->getDistrict();
+      $array['shippingAddressCity'] = $this->getCity();
+      $array['shippingAddressPostalCode'] = $this->getPostalCode();
+      if($this->getComplement())
+        $array['shippingAddressComplement'] = $this->getComplement();
+
+      $array['shippingCost'] = $this->getCost();
+      return $array;
+    } else {
+      return parent::toArray();
+    }
+  }
+}
